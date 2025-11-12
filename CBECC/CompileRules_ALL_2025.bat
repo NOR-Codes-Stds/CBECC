c5576b0e734c25413ea730cf7199756be02a5947
Rob Hitchcock <robHitchcock@users.noreply.github.com>
2025-11-12 11:03:00 -0800

    Merge pull request #597 from NOR-Codes-Stds/RJH2-Issue214
    Updates contained in this dev branch "RJH2-Issue214" were not merged into the recent CBECC 2025 release. They are now merged for the next release.

--- CBECC/CompileRules_ALL_2025.bat
+++ CBECC/CompileRules_ALL_2025.bat
@@ -14,6 +14,7 @@ copy "..\RulesetSrc\T24NRMF\RTF\*.*" "..\CBECC\Data\Rulesets\T24_2025\RTF\*.*"
 copy "..\RulesetSrc\shared\RTF\*.*" "..\CBECC\Data\Rulesets\T24_2025\RTF\*.*"
 copy "..\RulesetSrc\shared\Screens_Res_2025.txt" "..\CBECC\Data\Rulesets\T24_2025\*.*"
 copy "..\RulesetSrc\shared\*.jpg" "..\CBECC\Data\Rulesets\T24_2025\*.*"
+copy "..\RulesetSrc\shared\*.png" "..\CBECC\Data\Rulesets\T24_2025\*.*"
 rem copy "..\RulesetSrc\CEC 2013 Nonres\CEC 2013 NonRes Defaults.dbd" "..\CBECC-Com\Data\Rulesets\T24_2025\T24_2025 Defaults.dbd"
 rem copy "..\RulesetSrc\T24NRMF\DHWDU2.txt" "..\CBECC\CSE\*.*"
 goto :done
@@ -30,7 +31,7 @@ echo -
 echo off
   echo --------------------------------------------
   echo Compiling 2025 SFam ruleset...
-if exist BEMCompiler25.exe BEMCompiler25.exe --bemBaseTxt="../RulesetSrc/BEMBase-SFam.txt" --bemEnumsTxt="../RulesetSrc/T24SFam/CAR25 BEMEnums.txt" --bemBaseBin="Data/Rulesets/CA Res 2025/CAR25 BEMBase.bin" --rulesTxt="../RulesetSrc/T24SFam/Rules-2025.txt" --rulesBin="Data/Rulesets/CA Res 2025.bin" --rulesLog="_Rules-SFam-2025 Log.out" --compileDM --compileRules
+if exist BEMCompiler25.exe BEMCompiler25.exe --sharedPath1="../RulesetSrc/shared/" --bemBaseTxt="../RulesetSrc/BEMBase-SFam.txt" --bemEnumsTxt="../RulesetSrc/T24SFam/CAR25 BEMEnums.txt" --bemBaseBin="Data/Rulesets/CA Res 2025/CAR25 BEMBase.bin" --rulesTxt="../RulesetSrc/T24SFam/Rules-2025.txt" --rulesBin="Data/Rulesets/CA Res 2025.bin" --rulesLog="_Rules-SFam-2025 Log.out" --compileDM --compileRules
 echo BEMCompiler25.exe returned (%ERRORLEVEL%) for CA Res 2025
 if %ERRORLEVEL%==0 goto :copyfiles2
 goto :error2
@@ -38,6 +39,8 @@ goto :error2
 copy "..\RulesetSrc\T24SFam\CAR25 Screens.txt"  "Data\Rulesets\CA Res 2025\*.*"
 copy "..\RulesetSrc\T24SFam\T24R ToolTips.txt" "Data\Rulesets\CA Res 2025\CAR25 ToolTips.txt"
 copy "..\RulesetSrc\T24SFam\RTF\*.*" "Data\Rulesets\CA Res 2025\RTF\*.*"
+copy "..\RulesetSrc\shared\*.jpg" "Data\Rulesets\CA Res 2025\*.*"
+copy "..\RulesetSrc\shared\*.png" "Data\Rulesets\CA Res 2025\*.*"
 rem copy "..\RulesetSrc\T24SFam\DHWDU2.txt" "CSE\*.*"
 goto :finalDone
 :error2
